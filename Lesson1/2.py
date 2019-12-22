24d3d49cf144b9604ac38e51e76c9f555c53776c
Sergey Romanov <blog.langepass@gmail.com>
2019-12-22 18:59:41 +0300

    обновил немного задания после рассмотрения урока

--- Lesson1/2.py
+++ Lesson1/2.py
@@ -1,6 +1,6 @@
-time = int(input('ВВедите время в секундах: '))
+time = int(input('Введите время в секундах: '))
 hours = (time // 3600) % 24
 minutes = (time // 60) % 60
 seconds = time % 60
 
-print(f'Время в формате чч:мм:сс: {str(hours).rjust(2, "0")}:{str(minutes).rjust(2,"0")}:{str(seconds).rjust(2, "0")}')
+print(f'Время в формате чч:мм:сс: {hours:02}:{minutes:02}:{seconds:02}')
